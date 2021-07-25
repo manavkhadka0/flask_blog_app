3acf883d137eef3fb8b8dbcae72a94b444dae686
Manav Khadka <manavkhadka0@gmail.com>
2021-07-25 16:24:59 +0545

    layout completed

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.templating import render_template
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
